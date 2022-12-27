e03aef92d4601c220f9cda75ebe31bd3daf51950
AlinaLuninaVyacheslavovna <118982783+AlinaLuninaVyacheslavovna@users.noreply.github.com>
2022-12-28 01:55:53 +0300

    Update task4.py

--- Лабораторная работа №6/task4.py
+++ Лабораторная работа №6/task4.py
@@ -4,22 +4,11 @@ INPUT_FILE = "input.csv"
 
 
 def csv_to_list_dict(filename, delimiter=',', new_line='\n'):
-    with open(filename) as f:
-        list_ = []
-        for index, line in enumerate(f):
-
-            fields = line.strip(new_line).split(delimiter)
-
-            if index == 0:
-                heads = fields
-                continue
-
-            list_.append({})
-
-            for i, _ in enumerate(heads):
-
-                list_[-1][heads[i]] = fields[i]
-    return list_
+    with open(filename, "r") as f:
+        lines = f.read().split(new_line)
+        fields = lines[0].split(delimiter)
+        data = [dict(zip(fields, line.split(delimiter))) for line in lines[1:]]
+    return json.dumps(data, indent=4)
 
 
-print(json.dumps(csv_to_list_dict(INPUT_FILE), indent=4))
+print(csv_to_list_dict(INPUT_FILE))
